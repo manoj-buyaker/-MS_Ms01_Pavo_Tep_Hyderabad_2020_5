a33d03ea8707c68848005caca86d44210c5d42ea
Vinod <raj.vinod727@gmail.com>
2020-05-05 14:00:40 +0530

    milestone 1 added catageory

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// since we are requiring the top level of faker, load all locales by default
-var Faker = require('./lib');
-var faker = new Faker({ locales: require('./lib/locales') });
-module['exports'] = faker;
+ // since we are requiring the top level of faker, load all locales by default
+ var Faker = require('./lib');
+ var faker = new Faker({ locales: require('./lib/locales') });
+ module['exports'] = faker;
+ 
+ console.log(faker.education.education());
